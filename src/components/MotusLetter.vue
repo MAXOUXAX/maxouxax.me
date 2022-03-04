4d27b529fca3d557c3808156c3bfb01ec3e63d1c
maxouxax <24844231+MAXOUXAX@users.noreply.github.com>
2022-03-04 13:32:24 +0100

    WIP: Motus

---
+++ src/components/MotusLetter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="motus-letter">
+        <span>{{ letter ? letter : '.' }}</span>
+        <div v-if="wellPlaced" class="red-square"></div>
+        <div v-if="wronglyPlaced" class="yellow-circle"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            letter: String,
+            wellPlaced: Boolean,
+            wronglyPlaced: Boolean
+        }
+    }
+    name: 'MotusLetter'
+}
+</script>
+
+<style scoped>
+.motus-letter{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+}
+.motus-letter span{
+    font-size: 4rem;
+}
+.motus-letter .red-square{
+    width: 100%;
+    height: 100%;
+    z-index: calc(inherit - 1);
+    background-color: red;
+}
+.motus-letter .yellow-circle{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    z-index: calc(inherit - 1);
+    background-color: yellow;
+}
+</style>
--- src/views/Motus.vue
+++ src/views/Motus.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container>
         <v-row v-for="i in 8" :key="i">
-            <v-col v-for="i in 8" :key="i">
-                <p>TEST {{ n }} x {{ i }}</p>
+            <v-col width="64px" height="64px" class="pa-0" v-for="i in 8" :key="i">
+                <motus-letter></motus-letter>
             </v-col>
         </v-row>
     </v-container>
@@ -10,10 +10,16 @@
 
 <script>
 export default {
-    name: 'Motus'
+    name: 'Motus',
+    components: {
+        'motus-letter': MotusLetter
+    }
 }
+
 </script>
 
-<style>
-
+<style scoped>
+.col{
+    border: 0.5px solid white;
+}
 </style>
