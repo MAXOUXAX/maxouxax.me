39604815e95b87d56b81f02278e9ce0f57040a98
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-03-08 18:33:06 +0100

    Make get-random-word actually get a random word

--- functions/api/motus/get-random-word.js
+++ functions/api/motus/get-random-word.js
@@ -1,18 +1,12 @@
 export async function onRequestGet(context) {
-	const {
-		request,
-		env,
-		params,
-		waitUntil,
-		next,
-		data,
-	} = context;
-
 	let words = [];
 	await fetch("https://framagit.org/JonathanMM/sutom/-/raw/main/data/mots.txt").then(response => response.text()).then(data => {
 		data.slice(0, -1).split("\n").forEach(word => {
 			words.push(word);
 		});
 	});
-	return new Response(words[0]);
+	words.map(word => word.toLowerCase())
+		.map(word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+		.map(word => word.trim());
+	return new Response(words[Math.floor(Math.random() * words.length)]);
 }
