acfe7f1cd8d63f68f2f0982fb92a883f980ccb5a
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-03-08 21:34:15 +0100

    API enhance

--- functions/api/motus/get-random-word.js
+++ functions/api/motus/get-random-word.js
@@ -1,11 +1,18 @@
+const wordList = "https://framagit.org/JonathanMM/sutom/-/raw/main/data/mots.txt";
+
 export async function onRequestGet(context) {
 	let words = [];
-	await fetch("https://framagit.org/JonathanMM/sutom/-/raw/main/data/mots.txt").then(response => response.text()).then(data => {
+	await fetch(wordList, {
+		cf: {
+			cacheEverything: true,
+			cacheTtl: "86400"
+		}
+	}).then(response => response.text()).then(data => {
 		data.slice(0, -1).split("\n").forEach(word => {
 			words.push(word);
 		});
 	});
-	words.map(word => word.toLowerCase())
+	words = words.map(word => word.toLowerCase())
 		.map(word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
 		.map(word => word.trim());
 	return new Response(words[Math.floor(Math.random() * words.length)]);
--- functions/api/motus/validate-word.js
+++ functions/api/motus/validate-word.js
@@ -1,15 +1,46 @@
+const wordList = "https://framagit.org/JonathanMM/sutom/-/raw/main/data/mots.txt";
+
 export async function onRequestPost(request) {
 	let words = [];
-	await fetch("https://framagit.org/JonathanMM/sutom/-/raw/main/data/mots.txt").then(response => response.text()).then(data => {
+	let response = {};
+	await fetch(wordList, {
+		cf: {
+			cacheEverything: true,
+			cacheTtl: "86400"
+		}
+	}).then(response => response.text()).then(data => {
 		data.slice(0, -1).split("\n").forEach(word => {
 			words.push(word);
 		});
 	});
-	//check if the words array contains request.
-	words.forEach(word => {
-		if (word === request.body) {
-			return new Response(true);
+	words = words.map(word => word.toLowerCase())
+		.map(word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+		.map(word => word.trim());
+	if (request.body instanceof FormData) {
+		let word = request.body.get("word");
+		if (word) {
+			response.push({
+				valid: words.includes(word) ? true : false,
+				word: word
+			});
+		}else{
+			response.push({
+				valid: false,
+				errorMessage: "No word provided",
+				word: ""
+			});
 		}
+	}else{
+		response.push({
+			valid: false,
+			errorMessage: "Body does not match valid type",
+			word: ""
+		});
+	}
+	let json = JSON.stringify(response, null, 2);
+	return new Response(json, {
+		headers: {
+			'content-type': 'application/json;charset=UTF-8',
+		},
 	});
-	return new Response(false);
 }
