c6e9a900f9975b0dec43606067aafdf7c7cb6cc1
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-03-25 17:38:46 +0100

    Added some padding on announcement and made it wider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,7 @@
         </v-btn>
       </router-link>
     </div>
-    <div class="announcement">
+    <div class="announcement pa-6">
       <iframe width="100%" height="150" src="https://odesli.co/embed/?url=https%3A%2F%2Fsong.link%2Fmaxouxax-mess&theme=dark" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>
     </div>
   </div>
@@ -52,7 +52,7 @@ export default {
 }
 .announcement{
   width: 100%;
-  max-width: 480px;
+  max-width: 680px;
   display: flex;
   justify-content: center;
   align-items: center;
