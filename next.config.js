50eccbe9bb480d2e370b6f9f9ca6a459d9b03405
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-12-02 19:08:07 +0100

    refactor: remove github project files and separate shadcn/ui theme form globals.css

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    viewTransition: true,
+  },
+};
 
 export default config;
